84cb79dbce196734161869256e6ea4ab7616d790
acus <agusnurhidayat900@gmail.com>
2021-08-06 23:09:30 +0700

    Upload file

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Header = () => {
+
+    return <h1 className="main-heading">Spotify Music Search</h1>;
+
+};
+
+export default Header;
---
+++ src/components/RedirectPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const RedirectPage = () => {
+    return <div>RedirectPage</div>;
+};
+export default RedirectPage;
